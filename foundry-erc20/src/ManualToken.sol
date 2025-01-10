e55e2305377f970f63889b41d5a7235a88ed74a9
luja <lujachitrakar156@gmail.com>
2025-01-10 09:06:06 +0000

    deploy ERC20 token to anvil

---
+++ foundry-erc20/src/ManualToken.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.18;
+
+contract ManualToken {
+    mapping(address => uint) private s_balances;
+
+    function name() public pure returns (string memory) {
+        return "ManualTokenn";
+    }
+
+    function totalSupply() public pure returns (uint256) {
+        return 100 ether;
+    }
+
+    function decimals() public pure returns (uint) {
+        return 18;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256) {
+        return s_balances[_owner];
+    }
+
+    function transter(address _to, uint256 _amount) public {
+        uint256 previousBalance = balanceOf(msg.sender) + balanceOf(_to);
+        s_balances[msg.sender] -= _amount;
+        s_balances[_to] += _amount;
+        require(balanceOf(msg.sender) + balanceOf(_to) == previousBalance);
+    }
+}
