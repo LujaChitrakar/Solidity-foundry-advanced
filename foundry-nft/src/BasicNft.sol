f59b8962172f5cfd992e5118b1a9639b551e94b2
luja <lujachitrakar156@gmail.com>
2025-01-20 09:16:26 +0000

    dynamic nft

--- foundry-nft/src/BasicNft.sol
+++ foundry-nft/src/BasicNft.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier:MIT
 pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
     uint256 private s_tokenCounter;
     mapping(uint256 => string) private s_tokenIdToUri;
 
-    constructor() ERC721("Basic NFT", "DOG") {
+    constructor() ERC721("Doggie", "DOG") {
         s_tokenCounter = 0;
     }
 
