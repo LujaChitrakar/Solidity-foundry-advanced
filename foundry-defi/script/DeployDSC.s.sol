d019feec071fe22c19145822d4d0d6da6c44a39b
luja <lujachitrakar156@gmail.com>
2025-02-04 06:24:06 +0000

    writing tests

--- foundry-defi/script/DeployDSC.s.sol
+++ foundry-defi/script/DeployDSC.s.sol
@@ -10,7 +10,10 @@ contract DeployDSC is Script {
     address[] public tokenAddresses;
     address[] public pricefeedAddresses;
 
-    function run() external returns (DecentralizedStableCoin, DSCEngine) {
+    function run()
+        external
+        returns (DecentralizedStableCoin, DSCEngine, HelperConfig)
+    {
         HelperConfig config = new HelperConfig();
 
         (
@@ -33,6 +36,6 @@ contract DeployDSC is Script {
         );
         dsc.transferOwnership(address(engine));
         vm.stopBroadcast();
-        return (dsc, engine);
+        return (dsc, engine, config);
     }
 }
